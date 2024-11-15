4d208d9570159d61cbc7f25570d11c2d97acd46f
him8033 <himanshu8033@gmail.com>
2024-11-15 17:16:23 +0530

    exports method with whole directory

--- first.js
+++ first.js
@@ -1,16 +1,8 @@
-// for(let i=0;i<5;i++){
-//     console.log("Hello",i);
-// }
-// console.log(process.argv);
-// let args=process.argv;
-// for(i=2;i<args.length;i++){
-//     console.log("Hello to, ",args[i]);
-// }
-
-let math = require("./math");
-console.log(math.sum(10,5));
-console.log(math.mul(2,5));
-console.log(math.divide(10,5));
-console.log(math.sub(10,5));
-console.log(math.pi());
-console.log(math.g());
+for(let i=0;i<5;i++){
+    console.log("Hello",i);
+}
+console.log(process.argv);
+let args=process.argv;
+for(i=2;i<args.length;i++){
+    console.log("Hello to, ",args[i]);
+}
--- script.js
+++ script.js
@@ -5,3 +5,7 @@ console.log(math.divide(10,5));
 console.log(math.sub(10,5));
 console.log(math.pi());
 console.log(math.g());
+
+const fruitItem = require("./fruits");
+console.log(fruitItem);
+console.log(fruitItem[0].name);
